8c9c46567f7942a619ecc02b8e9e37ec2394d744
Dennis Waldherr <bytekeeper@mailbox.org>
2023-11-03 13:52:22 +0100

    Now runs as a server and uses library to access github

--- src/main.rs
+++ src/main.rs
@@ -1,256 +1,45 @@
-use base64::encode;
-use hyper::Response;
-use rand::prelude::*;
-use reqwest::header;
+use anyhow::Context;
 use serde::{Deserialize, Serialize};
-use serde_yaml;
-use std::collections::HashMap;
-use std::env;
-use std::io::{stdin, stdout, Read, Write};
-use std::time::SystemTime;
+use std::sync::OnceLock;
 
+mod server;
+
+/// This is the request from a client
 #[derive(Deserialize, Debug)]
-struct Post {
-    r#ref: String,
+pub struct Post {
+    path: String,
     message: String,
     name: String,
     url: String,
-    redirect: String,
 }
 
+/// This will be serialized into a comment file on GitHub
 #[derive(Serialize, Debug)]
-struct Comment {
-    id: String,
-    r#ref: String,
-    message: String,
-    name: String,
-    url: String,
+struct Comment<'a> {
+    id: &'a str,
+    message: &'a str,
+    name: &'a str,
+    url: &'a str,
     date: u64,
 }
 
-#[derive(Serialize)]
-struct CreateRef {
-    r#ref: String,
-    sha: String,
+#[derive(Deserialize)]
+pub struct GitHubConfig {
+    pub token: String,
+    pub owner: String,
+    pub repo: String,
 }
 
-#[derive(Serialize)]
-struct CreateFile {
-    message: String,
-    content: String,
-    branch: String,
-    committer: UserRef,
-}
+pub static CONFIG: OnceLock<GitHubConfig> = OnceLock::new();
 
-#[derive(Serialize)]
-struct UserRef {
-    name: String,
-    email: String,
-}
-
-#[derive(Serialize)]
-struct CreatePR {
-    title: String,
-    head: String,
-    base: String,
-}
-
-struct GitHub<'a> {
-    client: reqwest::blocking::Client,
-    owner: &'a str,
-    repo: &'a str,
-}
-
-const GITHUB_BASE_URL : &str = "https://api.github.com/repos/";
-
-impl GitHub<'_> {
-    fn new<'a>(token: &'a str, owner: &'a str, repo: &'a str) -> GitHub<'a> {
-        let mut headers = header::HeaderMap::new();
-        headers.insert(
-            header::ACCEPT,
-            header::HeaderValue::from_static("application/vnd.github.v3+json"),
-        );
-        headers.insert(
-            header::USER_AGENT,
-            header::HeaderValue::from_static("Comment-Bridger"),
-        );
-        headers.insert(
-            header::AUTHORIZATION,
-            header::HeaderValue::from_str(token).unwrap(),
-        );
-
-        let client = reqwest::blocking::Client::builder()
-            .default_headers(headers)
-            .build()
-            .unwrap();
-
-        GitHub {
-            owner,
-            repo,
-            client,
-        }
-    }
-
-    fn get_branch(&self, branch: &str) -> reqwest::Result<reqwest::blocking::Response> {
-        let url = url::Url::parse(
-            format!(
-                "{}{}/{}/branches/{}",
-                GITHUB_BASE_URL, self.owner, self.repo, branch
-            )
-            .as_str(),
-        )
-        .unwrap();
-        self.client.get(url).send()
-    }
-
-    fn create_ref(&self, create_ref: &CreateRef) -> reqwest::Result<reqwest::blocking::Response> {
-        let url = url::Url::parse(
-            format!(
-                "{}{}/{}/git/refs",
-                GITHUB_BASE_URL, self.owner, self.repo
-            )
-            .as_str(),
-        )
-        .unwrap();
-        self.client.post(url).json(create_ref).send()
-    }
-
-    fn create_file(
-        &self,
-        path: &str,
-        create_file: &CreateFile,
-    ) -> reqwest::Result<reqwest::blocking::Response> {
-        let url = url::Url::parse(
-            format!(
-                "{}{}/{}/contents/{}",
-                GITHUB_BASE_URL, self.owner, self.repo, path
-            )
-            .as_str(),
-        )
-        .unwrap();
-        self.client.put(url).json(create_file).send()
-    }
-
-    fn create_pr(&self, create_pr: &CreatePR) -> reqwest::Result<reqwest::blocking::Response> {
-        let url = url::Url::parse(
-            format!(
-                "{}{}/{}/pulls",
-                GITHUB_BASE_URL, self.owner, self.repo
-            )
-            .as_str(),
-        )
-        .unwrap();
-        self.client.post(url).json(create_pr).send()
-    }
-}
-
-fn main() {
-    let token = &env::var("TOKEN").expect("TOKEN missing");
-    let owner = &env::var("REPO_OWNER").expect("REPO_OWNER missing");
-    let owner_email = &env::var("REPO_OWNER_EMAIL").expect("REPO_OWNER_EMAIL missing");
-    let repo = &env::var("REPO").expect("REPO missing");
-
-    let content_length: usize = env::var("CONTENT_LENGTH")
-        .ok()
-        .and_then(|cl| cl.parse::<usize>().ok())
-        .expect("Content length expected");
-    assert!(content_length < 100 * 1024, "Comment size limit exceeded");
-    let mut body = vec![0; content_length];
-    stdin().read_exact(&mut body).expect("Could not read body");
-
-    let post: Post = serde_urlencoded::from_bytes(body.as_slice()).unwrap();
-
-    let github = GitHub::new(token, owner, repo);
-    let master = &github
-        .get_branch("master")
-        .unwrap()
-        .json::<HashMap<String, serde_json::Value>>()
-        .unwrap()["commit"]["sha"];
-    if let serde_json::Value::String(sha) = master {
-        let time = SystemTime::now()
-            .duration_since(SystemTime::UNIX_EPOCH)
-            .unwrap()
-            .as_secs();
-        let mut rng = thread_rng();
-        let comment_id = format!("{}_{}", time, rng.gen_range(0, 999999999));
-
-        github
-            .create_ref(&CreateRef {
-                r#ref: format!("refs/heads/comments/{}", comment_id),
-                sha: sha.to_string(),
-            })
-            .unwrap();
-
-        let comment_file = &format!("_data/comments/{}/{}.yaml", post.r#ref, comment_id);
-        let branch_name = format!("comments/{}", comment_id);
-
-        let comment = Comment {
-            id: comment_id.to_string(),
-            r#ref: post.r#ref.to_string(),
-            message: post.message.to_string(),
-            name: post.name.to_string(),
-            url: post.url.to_string(),
-            date: time,
-        };
-
-        github
-            .create_file(
-                comment_file,
-                &CreateFile {
-                    message: "Comment".to_string(),
-                    content: encode(&serde_yaml::to_string(&comment).unwrap()),
-                    branch: branch_name.to_string(),
-                    committer: UserRef {
-                        name: owner.to_string(),
-                        email: owner_email.to_string(),
-                    },
-                },
-            )
-            .unwrap();
-
-        github
-            .create_pr(&CreatePR {
-                title: format!("Comment {}", &comment_id),
-                head: branch_name.to_string(),
-                base: "master".to_string(),
-            })
-            .unwrap();
-    } else {
-        panic!("Invalid ref!");
-    }
-    let response = Response::builder()
-        .version(hyper::Version::HTTP_11)
-        .status(hyper::StatusCode::SEE_OTHER)
-        .header("Location", post.redirect)
-        .body(())
-        .unwrap();
-    let buf = response_to_buf(response);
-    stdout().write_all(&buf).unwrap();
-}
-
-fn response_to_buf(response: Response<()>) -> Vec<u8> {
-    let mut output = String::new();
-    output.push_str("Status: ");
-    output.push_str(response.status().as_str());
-    if let Some(reason) = response.status().canonical_reason() {
-        output.push_str(" ");
-        output.push_str(reason);
-    }
-    output.push_str("\n");
-
-    {
-        let headers = response.headers();
-        let mut keys: Vec<&hyper::header::HeaderName> = headers.keys().collect();
-        keys.sort_by_key(|h| h.as_str());
-        for key in keys {
-            output.push_str(key.as_str());
-            output.push_str(": ");
-            output.push_str(headers.get(key).unwrap().to_str().unwrap());
-            output.push_str("\n");
-        }
-    }
-
-    output.push_str("\n");
-
-    output.into_bytes()
+fn main() -> anyhow::Result<()> {
+    env_logger::init();
+    let Ok(_) = CONFIG.set(
+        serde_yaml::from_slice(&std::fs::read("config.yaml").context("Loading config file")?)
+            .context("Parsing config file")?,
+    ) else {
+        panic!("Could not set config")
+    };
+    server::main()?;
+    Ok(())
 }
---
+++ src/server.rs
@@ -0,0 +1,160 @@
+use crate::{Comment, Post, CONFIG};
+use core::convert::Infallible;
+use http_body::Limited;
+use hyper::header::{
+    ACCESS_CONTROL_ALLOW_HEADERS, ACCESS_CONTROL_ALLOW_METHODS, ACCESS_CONTROL_ALLOW_ORIGIN,
+};
+use hyper::service::{make_service_fn, service_fn};
+use hyper::{Body, Request, Response, Server, StatusCode};
+use log::{debug, info};
+use octocrab::models::repos::{CommitAuthor, Object};
+use octocrab::params::repos::Reference;
+use rand::{thread_rng, Rng};
+use std::fmt::Write;
+use std::net::SocketAddr;
+use std::time::SystemTime;
+
+async fn post_comment_service(req: Request<Body>) -> hyper::Result<Response<Body>> {
+    if req.method() == hyper::Method::OPTIONS {
+        return Ok(Response::builder()
+            .status(StatusCode::NO_CONTENT)
+            .header(ACCESS_CONTROL_ALLOW_METHODS, "OPTIONS, POST")
+            .header(ACCESS_CONTROL_ALLOW_ORIGIN, "*")
+            .header(ACCESS_CONTROL_ALLOW_HEADERS, "content-type")
+            .body("".into())
+            .unwrap());
+    }
+    // Prevent crashing the service by simple DDOS attacks
+    let body = Limited::new(req.into_body(), 100 * 1024);
+    let Ok(post_request) = hyper::body::to_bytes(body).await else {
+        return Ok(Response::builder()
+            .status(StatusCode::BAD_REQUEST)
+            .body("Comment size limit exceeded".into())
+            .unwrap());
+    };
+    let Ok(post): Result<Post, _> = serde_json::from_slice(&*post_request) else {
+        return Ok(Response::builder()
+            .status(StatusCode::BAD_REQUEST)
+            .body("Invalid JSON".into())
+            .unwrap());
+    };
+
+    let time = SystemTime::now()
+        .duration_since(SystemTime::UNIX_EPOCH)
+        .unwrap()
+        .as_secs();
+    let comment_id = format!("{}_{}", time, thread_rng().gen_range(0..999999999));
+
+    let comment = Comment {
+        id: &comment_id,
+        message: &post.message,
+        name: &post.name,
+        url: &post.url,
+        date: time,
+    };
+
+    let oc = octocrab::instance();
+    let branch_name = format!("comments/{}", comment_id);
+    let config = CONFIG.get().unwrap();
+    let path = format!("content{}comments.yaml", post.path);
+
+    let repo = oc.repos(&config.owner, &config.repo);
+
+    let master_sha = match repo
+        .get_ref(&Reference::Branch("master".to_string()))
+        .await
+        .expect("Could not get master ref")
+        .object
+    {
+        Object::Commit { sha, .. } | Object::Tag { sha, .. } => sha,
+        _ => unreachable!(),
+    };
+
+    debug!("Creating branch {} from {}", branch_name, master_sha);
+    repo.create_ref(&Reference::Branch(branch_name.clone()), master_sha)
+        .await
+        .expect("Could not create branch");
+
+    debug!("Requesting {}", path);
+    let content_items = match repo.get_content().path(&path).send().await {
+        Ok(content_items) => content_items,
+        Err(_) => {
+            return Ok(Response::builder()
+                .status(hyper::StatusCode::BAD_REQUEST)
+                .body(format!("Invalid path: {}", path).into())
+                .unwrap());
+        }
+    };
+    assert!(content_items.items.len() <= 1);
+    let content = content_items.items.iter().next();
+    let new_comment =
+        serde_yaml::to_string(&[&comment]).expect("Could not convert comment to yaml");
+    let author = CommitAuthor {
+        name: "Comment0r".to_string(),
+        email: "none@example.com".to_string(),
+    };
+
+    if let Some(content) = content {
+        let (mut content, sha) = (content.decoded_content().unwrap(), content.sha.clone());
+        writeln!(&mut content, "{}", new_comment).expect("Could not add comment to file");
+        debug!("Found existing file at {} with sha {}", path, sha);
+        repo.update_file(
+            &path,
+            format!("Added comment from '{}'", comment.name),
+            content,
+            sha,
+        )
+        .branch(&branch_name)
+        .commiter(author)
+        .send()
+        .await
+        .expect("Could not update file");
+    } else {
+        debug!("Creating new file at {}", path);
+        repo.create_file(
+            &path,
+            format!("Added comment from '{}'", comment.name),
+            new_comment,
+        )
+        .branch(&branch_name)
+        .commiter(author)
+        .send()
+        .await
+        .expect("Could not create file");
+    }
+
+    oc.pulls(&config.owner, &config.repo)
+        .create(
+            format!("New comment from {}", comment.name),
+            branch_name,
+            "master",
+        )
+        .send()
+        .await
+        .expect("Could not create PR");
+    info!("New comment from {} added", comment.name);
+
+    Ok(Response::builder()
+        .status(hyper::StatusCode::SEE_OTHER)
+        .header(ACCESS_CONTROL_ALLOW_METHODS, "OPTIONS, POST")
+        .header(ACCESS_CONTROL_ALLOW_ORIGIN, "*")
+        .header(ACCESS_CONTROL_ALLOW_HEADERS, "content-type")
+        .body("That worked, now goeth forth".into())
+        .unwrap())
+}
+
+#[tokio::main]
+pub async fn main() -> anyhow::Result<()> {
+    octocrab::initialise(
+        octocrab::Octocrab::builder()
+            .personal_token(CONFIG.get().unwrap().token.clone())
+            .build()?,
+    );
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+
+    let post_comment_service =
+        make_service_fn(|_conn| async { Ok::<_, Infallible>(service_fn(post_comment_service)) });
+    let server = Server::bind(&addr).serve(post_comment_service);
+    Ok(server.await?)
+}
